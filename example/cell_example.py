7cfd720a9d172c26c83d130b8cb78d83c91347a3
Disaster <disaster_vip@163.com>
2026-04-08 16:13:35 +0800

    feat(doc): add example and ignore

---
+++ example/cell_example.py
@@ -0,0 +1,12 @@
+from typing import Callable
+
+
+def enclose_function() -> Callable[[], None]:
+    print("enclose function execute")
+    age =10
+    def local_func():
+        print("local_func:", age)
+    return local_func
+
+f = enclose_function()
+
